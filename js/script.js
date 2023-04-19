b3d71c858661ccbd4299f6f87befbdca7277e992
JoaoVithorMPV <126449601+JoaoVithorMPV@users.noreply.github.com>
2023-04-19 02:45:34 -0300

    a

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const canvasCtx = canvasEl.getContext("2d");
 
 const lineWidth = 15;
 const gapX = 10;
+const mouse = { x: 0, y: 0 };
 
 function setup() {
   canvasEl.width = canvasCtx.width = window.innerWidth;
@@ -15,7 +16,7 @@ const field = {
   h: window.innerHeight,
   draw: function () {
     canvasCtx.fillStyle = "#286047";
-    canvasCtx.fillRect(0, 0, this.w, this.y);
+    canvasCtx.fillRect(0, 0, this.w, this.h);
   },
 };
 
@@ -35,9 +36,45 @@ const leftPaddle = {
   y: field.h / 2,
   w: line.w,
   h: 200,
+  _move: function () {
+    this.y = mouse.y;
+  },
   draw: function () {
     canvasCtx.fillStyle = "#ffffff";
     canvasCtx.fillRect(this.x, this.y, line.w, this.h);
+
+    this._movie();
+  },
+};
+
+const rightPaddle = {
+  x: field.w - line.w - gapX,
+  y: field.h / 2,
+  w: line.w,
+  h: 200,
+  draw: function () {
+    canvasCtx.fillStyle = "#ffffff";
+    canvasCtx.fillRect(this.x, this.y, this.w, this.h);
+  },
+};
+
+const ball = {
+  //Desenho da bola
+  x: field.w / 2,
+  y: field.h / 2,
+  r: 20,
+  speed: 5,
+  _move: function () {
+    this.x += this.speed;
+    this.y += this.speed;
+  },
+  draw: function () {
+    canvasCtx.fillStyle = "#F0F72D";
+    canvasCtx.beginPath();
+    canvasCtx.arc(this.x, this.y, 20, 0, 2 * Math.PI, false);
+    canvasCtx.fill();
+
+    this._move();
   },
 };
 
@@ -45,17 +82,31 @@ function draw() {
   field.draw();
   line.draw();
   leftPaddle.draw();
+  rightPaddle.draw();
+  ball.draw();
+}
 
-  //Desenho da raquete direita
-  canvasCtx.fillStyle = "#ffffff";
-  canvasCtx.fillRect(window.innerWidth - lineWidth - gapX, 500, lineWidth, 200);
+window.animateFrame = (function () {
+  return (
+    window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function (callback) {
+      return window.setTimeout(callback, 1000 / 60);
+    }
+  );
+})();
 
-  //Desenho da bola
-  canvasCtx.fillStyle = "#ffffff";
-  canvasCtx.beginPath();
-  canvasCtx.arc(120, 240, 20, 0, 2 * Math.PI, false);
-  canvasCtx.fill();
+function main() {
+  animateFrame(main);
+  draw();
 }
 
 setup();
-draw();
+main();
+
+canvasEl.addEventListener("mousemove", function (e) {
+  (mouse.x = e.pageX), (mouse.y = e.pageY);
+});
